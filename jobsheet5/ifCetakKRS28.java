83f0366f47ca6ae352df0a02d2e5427a3523cd25
eldezaki <eldezaki>
2025-10-02 12:21:50 +0700

    Ternary Operator

--- jobsheet5/ifCetakKRS28.java
+++ jobsheet5/ifCetakKRS28.java
@@ -13,5 +13,12 @@ public class ifCetakKRS28 {
         } else {
             System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
         }
+
+        System.out.println(
+            uktLunas 
+            ? "Pembayaran UKT terverifikasi.\nSilahkan cetak KRS dan minta tanda tangan DPA"
+            : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu"
+        );
+
     }
 }
