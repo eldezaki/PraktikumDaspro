906d2d8c68854cbd80a32c63be2d0cb303633718
eldezaki <eldezaki>
2025-10-02 18:35:39 +0700

    Modifikasi Percobaan 2

--- jobsheet5/ifElseCetakKRS28.java
+++ jobsheet5/ifElseCetakKRS28.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 public class ifElseCetakKRS28 {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("--- Cetak KRS SIAKAD ---");
