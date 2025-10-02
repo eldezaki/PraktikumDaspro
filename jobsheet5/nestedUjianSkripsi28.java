828aa3371e11a6e4af586ba122db296f66d59ebe
eldezaki <eldezaki>
2025-10-02 12:11:08 +0700

    Fix Absen

--- jobsheet5/nestedUjianSkripsi28.java
+++ jobsheet5/nestedUjianSkripsi28.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class nestedUjianSkripsiNoPresensi {
+public class nestedUjianSkripsi28 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
--- jobsheet5/switchCetakKRS29.java
+++ jobsheet5/switchCetakKRS29.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class switchCetakKRSNoPresensi {
+public class switchCetakKRS28 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
